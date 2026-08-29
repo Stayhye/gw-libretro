c5ec07334d8b2e927c8082eca15c40efc673dd27
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-29 15:34:43 -0500

    Update libretro.c

--- src/libretro.c
+++ src/libretro.c
@@ -476,7 +476,7 @@ void retro_run(void)
                uint16_t b = (p >> 11) & 0x1F;
                
                // Re-pack with Red and Blue swapped (BGR layout) plus alpha/unused bit
-               dest[col] = (p & 0x8000) | (b << 10) | (g << 5) | r;
+               dest[col] = (b << 11) | (g << 5) | r;
             }
          }
          video_cb(pixels, out_width, out_height, out_width * sizeof( uint16_t ));
